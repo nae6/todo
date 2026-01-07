821b1bf9d85029b66d5821ae08e1ad426b7a4369
kanae nakaji <nae6p21@icloud.com>
2026-01-07 19:38:42 +0900

    TodoTest

--- src/database/factories/TodoFactory.php
+++ src/database/factories/TodoFactory.php
@@ -3,18 +3,19 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Todo;
 
 class TodoFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = Todo::class;
+
     public function definition()
     {
         return [
-            'content' => $this->faker->sentence(10)
+            'user_id' => User::factory(),
+            'content' => 'テスト用todo',
+            'is_done' => false,
         ];
     }
 }
